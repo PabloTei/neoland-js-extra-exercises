56a2ad5e32192b0a4342bb35578a0c43118cf5bb
PabloTei <pteijeirolopez@gmail.com>
2023-01-16 23:48:33 +0100

    iteration3

--- app.js
+++ app.js
@@ -167,13 +167,58 @@ const movies1 = [
 
 for (const movie of movies1) {
     for (const categorie of movie.categories) {
-        console.log(categorie)
-
+        
         if(moviesCategories.includes(categorie)){
-
         } else {
             moviesCategories.push(categorie)
         }
     }
 }
 console.log(moviesCategories);
+
+
+// 2.2 Dado el siguiente javascript usa forof y forin para hacer la media del volumen de todos los sonidos favoritos que tienen los usuarios.
+
+const users2 = [
+    {
+      name: "Alberto",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 50 },
+        rain: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Antonio",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 30 },
+        shower: { format: "ogg", volume: 55 },
+        train: { format: "mp3", volume: 60 },
+      },
+    },
+    {
+      name: "Pedro",
+      favoritesSounds: {
+        shower: { format: "mp3", volume: 50 },
+        train: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Cristina",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 67 },
+        wind: { format: "ogg", volume: 35 },
+        firecamp: { format: "mp3", volume: 60 },
+      },
+    },
+  ];
+
+  let volume = 0;
+
+  for (const user of users2) {
+    for (const key in user.favoritesSounds) {
+        volume += (user.favoritesSounds[key].volume);
+  }
+}
+console.log(volume);
